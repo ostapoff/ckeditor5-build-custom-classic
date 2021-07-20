aae7a1ed6d3bdbf3a8cd9dae02109d681836a93a
Oleksii Ostapov <ostapov@gmail.com>
2021-07-20 18:04:49 +0300

    add the cmslink custom plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -22,6 +22,7 @@ import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
+import Cmslink from '@ckeditor/ckeditor5-cmslink/src/cmslink';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
@@ -55,6 +56,7 @@ ClassicCustomEditor.builtinPlugins = [
 	ImageUpload,
 	Indent,
 	Link,
+	Cmslink,
 	List,
 	MediaEmbed,
 	Paragraph,
@@ -75,6 +77,7 @@ ClassicCustomEditor.defaultConfig = {
 			'bold',
 			'italic',
 			'link',
+			'cmslink',
 			'bulletedList',
 			'numberedList',
 			'|',
